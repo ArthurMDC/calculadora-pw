96e55a9dccc95f1f6f14d0e9a8d741338e2be6cc
Arthur Moura da Cunha <126520633+ArthurMDC@users.noreply.github.com>
2024-05-09 19:32:57 -0300

    Adicionando o localstorage

--- script.js
+++ script.js
@@ -1,26 +1,41 @@
+
+
 function insert(num)
 {
     var numero = document.getElementById('resultado').innerHTML;
     document.getElementById('resultado').innerHTML = numero + num;
+    saveToLocalStorage();
 }
 function clean()
 {
     document.getElementById('resultado').innerHTML = "";
+    saveToLocalStorage();
 }
 function back()
 {
     var resultado = document.getElementById('resultado').innerHTML;
     document.getElementById('resultado').innerHTML = resultado.substring(0, resultado.length -1);
+    saveToLocalStorage();
 }
-function calcular()
-{
+function calcular() {
     var resultado = document.getElementById('resultado').innerHTML;
-    if(resultado)
-    {
+    if (resultado) {
         document.getElementById('resultado').innerHTML = eval(resultado);
+    } else {
+        document.getElementById('resultado').innerHTML = "Nada...";
     }
-    else
-    {
-        document.getElementById('resultado').innerHTML = "Nada..."
+    saveToLocalStorage(); 
+} 
+
+function saveToLocalStorage() {
+    var resultado = document.getElementById('resultado').innerHTML;
+    localStorage.setItem('calc_resultado', resultado);
+}
+
+function loadFromLocalStorage() {
+    var resultado = localStorage.getItem('calc_resultado');
+    if (resultado) {
+        document.getElementById('resultado').innerHTML = resultado;
     }
+
 }
