0c861345592fb1a4172a83b1c28b93b7888ad8d6
Arthur Moura da Cunha <126520633+ArthurMDC@users.noreply.github.com>
2024-05-10 10:05:13 -0300

    alteração 10/05

--- script.js
+++ script.js
@@ -22,7 +22,7 @@ function calcular() {
     if (resultado) {
         document.getElementById('resultado').innerHTML = eval(resultado);
     } else {
-        document.getElementById('resultado').innerHTML = "Nada...";
+        document.getElementById('resultado').innerHTML = "Nenhum valor ";
     }
     saveToLocalStorage(); 
 } 
